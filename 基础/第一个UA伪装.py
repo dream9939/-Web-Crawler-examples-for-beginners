a1036bfb7516f4de8131e254226d5e245ee11d66
Daniel Zhang <glxx_2021_36_08@126.com>
2026-08-24 10:31:25 +0800

    Modify User-Agent header in request
    Updated User-Agent header to an empty string.

--- 基础/第一个UA伪装.py
+++ 基础/第一个UA伪装.py
@@ -2,7 +2,7 @@ import requests
 url="https://www.sogou.com/web?query=周杰伦"
 
 headers={
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.0.0"
+    "User-Agent": ""
 }
 resp=requests.get(url, headers=headers)
 print(resp.text)
